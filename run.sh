6a9a3fd1a05b88d5a450c1855a6e03b72bf2333a
Zeljko Obrenovic <zeljkoobrenovic@yahoo.com>
2025-05-03 01:27:42 +0200

    update

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ echo "$SOKRATES_CONFIG" > /app/analysis-scripts/config.json
 bash run-generate-scripts.sh
 bash run-execute-scripts.sh
 
-aws aws s3 cp /app/analysis-artifacts/reports/ $AWS_S3_EXPORT_URI --recursive
+aws s3 cp /app/analysis-artifacts/reports/ $AWS_S3_EXPORT_URI --recursive
