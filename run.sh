d89121eec1f4849c7a2974b43b2f5040b3e02d3b
Zeljko Obrenovic <zeljkoobrenovic@yahoo.com>
2025-05-02 22:57:14 +0200

    update

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ echo "$SOKRATES_CONFIG" > /app/analysis-scripts/config.json
 bash run-generate-scripts.sh
 bash run-execute-scripts.sh
 
-aws s3 sync /app/analysis-artifacts $AWS_S3_EXPORT_URI --delete
+aws s3 sync /app/analysis-artifacts/reports/ $AWS_S3_EXPORT_URI
