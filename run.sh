0cf84c9ff1f663efa85d3b63617f7e6a906db46f
Zeljko Obrenovic <zeljkoobrenovic@yahoo.com>
2025-05-02 15:05:28 +0200

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 bash run-generate-scripts.sh
 bash run-execute-scripts.sh
 
-aws s3 sync /app/analysis-artifacts s3://sokrates-gallery/openai_test --delete
+aws s3 sync /app/analysis-artifacts/reports/openai s3://sokrates-gallery/openai_test --delete
