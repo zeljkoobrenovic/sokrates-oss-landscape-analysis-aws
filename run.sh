e6be66a185229cd2884f3739c4200ec4d89a6a53
Zeljko Obrenovic <zeljkoobrenovic@yahoo.com>
2025-05-05 11:18:40 +0200

    update

--- run.sh
+++ run.sh
@@ -2,11 +2,12 @@ if [ -n "$COMMAND" ]; then
   # Check if the command is valid
   if [[ "$COMMAND" != "analyze-github-orgs" && "$COMMAND" != "analyze-git-repo" && "$COMMAND" != "update-landscape" ]]; then
     echo "Invalid command: $COMMAND"
+    bash "cmd-help.sh"
     exit 1
   fi
 
   # Run the command
-  bash "cmd-$COMMAND"
+  bash "cmd-$COMMAND.sh"
 
   exit 1
 fi
