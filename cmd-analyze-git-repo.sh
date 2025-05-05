1185a2487afcb2541917f4fe6b3c1c4c362cc053
Zeljko Obrenovic <zeljkoobrenovic@yahoo.com>
2025-05-05 16:21:59 +0200

    update

--- cmd-analyze-git-repo.sh
+++ cmd-analyze-git-repo.sh
@@ -5,7 +5,8 @@ git clone $GIT_REPO_URL temp
 cd temp
 
 echo "java -jar -Xmx40g /app/sokrates-LATEST.jar init"
-java -jar -Xmx40g /app/sokrates-LATEST.jar init -conventionsFile /app/analysis-scripts/scripts/analysis/sokrates_conventions.json -addLink $GIT_REPO_URL 'Repository'
+java -jar -Xmx40g /app/sokrates-LATEST.jar init -conventionsFile /app/analysis-scripts/scripts/analysis/sokrates_conventions.json -name "$REPO_NAME" -description "$REPO_DESCRIPTION" -addLink $GIT_REPO_URL 'GitHub Repo' -logoLink "$REPO_LOGO"
+
 
 echo "java -jar -Xmx40g /app/sokrates-LATEST.jar generateReports"
 java -jar -Xmx40g /app/sokrates-LATEST.jar generateReports -timeout 9999
