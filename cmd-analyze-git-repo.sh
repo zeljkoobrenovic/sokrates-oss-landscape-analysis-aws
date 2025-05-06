27dfb8916a07765af2fdb2999d86f9e66019ffba
Zeljko Obrenovic <zeljkoobrenovic@yahoo.com>
2025-05-06 20:49:15 +0200

    update

--- cmd-analyze-git-repo.sh
+++ cmd-analyze-git-repo.sh
@@ -12,6 +12,9 @@ git clone $GIT_REPO_URL temp
 
 cd temp
 
+echo "java -jar -Xmx40g /app/sokrates-LATEST.jar extractGitHistory"
+java -jar -Xmx40g /app/sokrates-LATEST.jar extractGitHistory
+
 echo "java -jar -Xmx40g /app/sokrates-LATEST.jar init"
 java -jar -Xmx40g /app/sokrates-LATEST.jar init -conventionsFile /app/analysis-scripts/scripts/analysis/sokrates_conventions.json -name "$REPO_NAME" -description "$REPO_DESCRIPTION" -addLink $GIT_REPO_URL 'GitHub Repo' -logoLink "$REPO_LOGO"
 
