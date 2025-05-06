1493f85a9bb4242d886f2e3579ade467c9b080bc
Zeljko Obrenovic <zeljkoobrenovic@yahoo.com>
2025-05-06 22:39:40 +0200

    update

--- cmd-init-landscape.sh
+++ cmd-init-landscape.sh
@@ -8,7 +8,7 @@ echo "zip -q -r all-reports.zip ."
 zip -q -r all-reports.zip .
 
 echo "aws s3 cp all-reports.zip S3_FOLDER_URI/all-reports.zip"
-aws s3 cp all-reports.zip S3_FOLDER_URI/all-reports.zip
+aws s3 cp all-reports.zip $S3_FOLDER_URI/all-reports.zip
 rm all-reports.zip
 
 echo "-jar -Xmx40g /app/sokrates-LATEST.jar updateLandscape"
