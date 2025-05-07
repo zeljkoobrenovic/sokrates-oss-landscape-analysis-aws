899350588ed8612186fb392115e12a2963618b2e
Zeljko Obrenovic <zeljkoobrenovic@yahoo.com>
2025-05-07 11:29:00 +0200

    update

--- cmd-init-landscape.sh
+++ cmd-init-landscape.sh
@@ -10,7 +10,17 @@ zip -q -r all-reports.zip .
 
 echo "aws s3 cp all-reports.zip S3_FOLDER_URI/all-reports.zip"
 aws s3 cp all-reports.zip $S3_FOLDER_URI/all-reports.zip
-rm all-reports.zip
+
+if [[ -z "${LANDSCAPE_NAME}" ]]; then
+  echo "Not using a pre-defined landscapoe settings"
+else
+  if [ -d /app/analysis-artifacts/$LANDSCAPE_NAME/_sokrates_landscape/ ]; then
+    rm -rf _sokrates_landscape
+    mkdir _sokrates_landscape
+    cp -r /app/analysis-artifacts/$LANDSCAPE_NAME/_sokrates_landscape/ _sokrates_landscape/
+  fi
+fi
+
 
 echo "-jar -Xmx40g /app/sokrates-LATEST.jar updateLandscape"
 java -jar -Xmx40g /app/sokrates-LATEST.jar updateLandscape
