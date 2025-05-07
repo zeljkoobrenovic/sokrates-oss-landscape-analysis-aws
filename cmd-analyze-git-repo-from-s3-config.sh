c4a783122cf3439f367fa2c21500ba8fe00a8e36
Zeljko Obrenovic <zeljkoobrenovic@yahoo.com>
2025-05-07 23:06:05 +0200

    update

--- cmd-analyze-git-repo-from-s3-config.sh
+++ cmd-analyze-git-repo-from-s3-config.sh
@@ -1,7 +1,10 @@
-if [[ -z "${PAYLOAD_ID}" ]]; then
+
+if [[ -z "${S3_PAYLOAD_UR}" ]]; then
   echo "PAYLOAD_ID not defined"
   exit 1
 fi
+
+aws s3 cp $S3_PAYLOAD_URI payload.json
 
 node /app/analysis-scripts/payload/create-payload-script.js
 bash temp-run-payload-analysis.sh
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 if [ -n "$COMMAND" ]; then
   # Check if the command is valid
-  if [[ "$COMMAND" != "analyze-github-orgs" && "$COMMAND" != "analyze-git-repo" && "$COMMAND" != "init-landscape"  && "$COMMAND" != "update-landscape"  && "$COMMAND" != "get-payloads" ]]; then
+  if [[ "$COMMAND" != "analyze-github-orgs" && "$COMMAND" != "analyze-git-repo" && "$COMMAND" != "init-landscape"  && "$COMMAND" != "update-landscape"  && "$COMMAND" != "get-payloads"   && "$COMMAND" != "analyze-git-repo-from-s3-config" ]]; then
     echo "Invalid command: $COMMAND"
     bash "cmd-help.sh"
     exit 1
