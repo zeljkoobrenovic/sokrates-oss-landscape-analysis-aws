2e72c97ddcb0e5b740ee50db0809097022ec5068
Zeljko Obrenovic <zeljkoobrenovic@yahoo.com>
2025-05-16 12:25:21 +0200

    gitlab

--- cmd-get-payloads-gitlab.sh
+++ cmd-get-payloads-gitlab.sh
@@ -2,6 +2,6 @@ mkdir -p /app/analysis-scripts/generated/payloads/cache
 
 echo "$SOKRATES_CONFIG" > /app/analysis-scripts/config.json
 
-bash run-generate-scripts.sh
+bash run-generate-scripts-gitlab.sh
 
 aws s3 cp /app/analysis-scripts/generated/payloads s3://sokrates-gallery/_payloads/ --recursive
