4f02cfb215dbc526c6ebfe739d679df6bf6f1fc9
Zeljko Obrenovic <zeljkoobrenovic@yahoo.com>
2025-05-17 10:44:05 +0200

    gitlab

--- analysis-artifacts/reports/gitlab-org/_sokrates_landscape/docs/md-to-html.sh
+++ analysis-artifacts/reports/gitlab-org/_sokrates_landscape/docs/md-to-html.sh
@@ -1,8 +1,4 @@
-pandoc summary.md -o summary.html \
+pandoc summary.md \
+  -o summary.html \
   --standalone \
-  --css=../style.css \
-  --metadata title="Sokrates Landscape" \
-  --metadata author="GitLab" \
-  --metadata date="$(date +%Y-%m-%d)" \
-  --metadata description="Sokrates Landscape" \
-  --metadata keywords="sokrates, landscape, gitlab, analysis"
+  --metadata date="$(date +%Y-%m-%d)"
