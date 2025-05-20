a685c05eec7a14f3fee7eb19f78518ec67902c9b
Zeljko Obrenovic <zeljkoobrenovic@yahoo.com>
2025-05-20 10:04:34 +0200

    update

--- cmd-update-landscape.sh
+++ cmd-update-landscape.sh
@@ -7,19 +7,10 @@ aws s3 cp s3://sokrates-gallery/$LANDSCAPE_NAME/all-reports.zip .
 echo "unzip -q all-reports.zip"
 unzip -q all-reports.zip
 
-cd apps/analysis-artifacts/reports/$LANDSCAPE_NAME
-
-ls -l
-
-cp -rf /app/analysis-artifacts/reports/$LANDSCAPE_NAME/* .
-
-pwd
-ls -l _sokrates_landscape/
+cp -rf /app/analysis-artifacts/reports/$LANDSCAPE_NAME/* /app/temp
 
 echo "-jar -Xmx40g /app/sokrates-LATEST.jar updateLandscape"
 java -jar -Xmx40g /app/sokrates-LATEST.jar updateLandscape
 
-echo "aws s3 cp _sokrates_landscape/ s3://sokrates-gallery/$LANDSCAPE_NAME/_sokrates_landscape/ --recursive"
-pwd
-ls -l _sokrates_landscape/
+echo "s3 cp _sokrates_landscape/ s3://sokrates-gallery/$LANDSCAPE_NAME/_sokrates_landscape/ --recursive"
 aws s3 cp _sokrates_landscape/ s3://sokrates-gallery/$LANDSCAPE_NAME/_sokrates_landscape/ --recursive
